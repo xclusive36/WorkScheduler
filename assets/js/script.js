fc060c4a30377cdd781565763887c5276ef23419
Joshua Cavell <xclusive36@gmail.com>
2023-02-27 16:16:25 -0500

    Classes set per current hour in workday in calendar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // array of work hours in 24-hour time
+
 const weekDays = [
   "Sunday",
   "Monday",
@@ -22,30 +24,53 @@ const months = [
   "December",
 ];
 
+const currentHour = dayjs().hour(); // gets current hour in 24-hour time
+
+const returnHourClass = (hour) => {
+  // function to set class for each hour
+  if (hour < currentHour) {
+    return "past";
+  } else if (hour > currentHour) {
+    return "future";
+  } else {
+    return "present";
+  }
+};
+
 const getDate = () => {
   const currentWeekDay = dayjs().day(); // gets day of current week
   const currentDay = dayjs().date(); // gets day of current month
   const currentMonth = dayjs().month(); // gets current month
 
-  const nth = function(d) {
+  const nth = function (d) {
     // function to add suffix to day of month
     // https://stackoverflow.com/questions/15397372/javascript-new-date-ordinal-st-nd-rd-th
-    if (d > 3 && d < 21) return 'th';
+    if (d > 3 && d < 21) return "th";
     switch (d % 10) {
-      case 1:  return `st`;
-      case 2:  return "nd";
-      case 3:  return "rd";
-      default: return "th";
+      case 1:
+        return `st`;
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
     }
-  }
+  };
 
-  return `${weekDays[currentWeekDay]}, ${months[currentMonth]} ${currentDay}${nth(currentDay)}`;
+  return `${weekDays[currentWeekDay]}, ${
+    months[currentMonth]
+  } ${currentDay}${nth(currentDay)}`;
 };
 
 $(document).ready(function () {
   // Run code when the DOM is ready
 
-  $("#currentDay").text(getDate()); // set variable for current day element
+  $("#currentDay").text(getDate()); // set date for current day element
+
+  $( ".time-block" ).each(function( index ) {
+    $(this).addClass(returnHourClass(workHours[index]));
+  });
 
   // get current day and display in header
 
